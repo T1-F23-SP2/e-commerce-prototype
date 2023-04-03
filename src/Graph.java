9d3f7c155d6f74dedf71b91651b2bff859b56223
Redacted for Privacy <52310748@hidden.mail>
2023-04-03 03:26:44 +0200

    "Burde gå i seng"

--- src/Graph.java
+++ src/Graph.java
@@ -6,12 +6,15 @@ import org.bson.Document;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class BarGraphExample extends JFrame {
     private Integer[] data;
     int price;
+   private String[] names;
 
 
     public void DB() {
@@ -24,13 +27,18 @@ class BarGraphExample extends JFrame {
         List<Document> results = collection.find(query).into(new ArrayList<>());
 
         List<Integer> dataList = new ArrayList<>();
+        List<String> NameList = new ArrayList<>();
         for (Document doc : results) {
             int price = doc.getInteger("Price");
+            String name = doc.getString("Name");
             dataList.add(price);
+            NameList.add(name);
         }
         data = dataList.toArray(new Integer[0]);
+        names = NameList.toArray(new String[0]);
         System.out.println(data[0].intValue());
         System.out.println("");
+        System.out.println(names[0]);
     }
 
     public BarGraphExample() {
@@ -47,49 +55,74 @@ class BarGraphExample extends JFrame {
                 int width = getWidth();
                 int height = getHeight();
                 //int barWidth = width / data.length;
-                int barWidth = 200;
+                int marginSize = 50;
+                int barWidth = (int) ((getWidth() - marginSize * 2) / (double) data.length) - 50;
+                // Add margin to top and bottom
+                int margin = 50;
+                height -= (2 * margin);
+
                 int maxDataValue = 80000;
                 for (int i = 0; i < data.length; i++) {
                     maxDataValue = Math.max(maxDataValue, data[i].intValue());
-                    System.out.println(" Height:  " + height);
-                    System.out.println(" Max:  " + maxDataValue);
-                    System.out.println("");
-
-
                 }
-                System.out.println(" Height:  " + height);
-                System.out.println(" Max:  " + maxDataValue);
 
                 int scale = (int) Math.ceil((double) height / maxDataValue);
 
-                System.out.println(" Scale:  " + scale);
-                g2.setColor(Color.BLUE);
+                // Draw bars
+                Random rand = new Random();
                 for (int i = 0; i < data.length; i++) {
-                    int x = i * barWidth;
-                    int y = height - (int) (data[i].intValue() * scale);
-                    int barHeight = (int) (data[i].intValue() * scale);
+                    int x = marginSize + i * (barWidth + 50);
+                    int y = height - data[i] * scale;
+                    int barHeight = data[i] * scale;
+                    Color color = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+                    g2.setColor(color);
                     g2.fillRect(x, y, barWidth, barHeight);
 
+                    // Add labels on x axis
+                    g2.setColor(Color.RED);
+                    g2.setFont(new Font("Arial", Font.BOLD, 12));
 
-                System.out.println(" i: " + i);
-                    System.out.println(" X: " + x);
+                        String productName = "Product " + (names[i]);
+                        int productNameWidth = g2.getFontMetrics().stringWidth(productName);
+                        int productNameX = x + (barWidth - productNameWidth) / 2;
+                        int productNameY = getHeight() - margin / 2;
+                        g2.drawString(productName, productNameX, productNameY);
 
-                    System.out.println(" Y:  " + y);
-
-                    System.out.println(" barWidth:  " + barWidth);
-
-                    System.out.println(" barHeight:  " + barHeight);
-
-
+                        // Add labels on y axis
+                        g2.setColor(Color.RED);
+                        g2.setFont(new Font("Arial", Font.BOLD, 12));
+                        String priceLabel = "$" + ( data[i]);
+                        int priceLabelWidth = g2.getFontMetrics().stringWidth(priceLabel);
+                        int priceLabelX = marginSize - priceLabelWidth - 5;
+                        int priceLabelY = y + barHeight / 2 + 5;
+                        g2.drawString(priceLabel, priceLabelX, priceLabelY);
 
                 }
-            }
 
+                // Draw x and y axis labels
+                g2.setColor(Color.RED);
+                g2.setFont(new Font("Arial", Font.BOLD, 14));
+                String xAxisLabel = "Product Names";
+                int xAxisLabelWidth = g2.getFontMetrics().stringWidth(xAxisLabel);
+                int xAxisLabelX = getWidth() / 2 - xAxisLabelWidth / 2;
+                int xAxisLabelY = getHeight() - margin / 4;
+                g2.drawString(xAxisLabel, xAxisLabelX, xAxisLabelY);
+
+                String yAxisLabel = "Price";
+                int yAxisLabelWidth = g2.getFontMetrics().stringWidth(yAxisLabel);
+                int yAxisLabelX = margin / 4;
+                int yAxisLabelY = getHeight() / 2 + yAxisLabelWidth / 2;
+                AffineTransform at = new AffineTransform();
+                at.rotate(-Math.PI / 2);
+                g2.setTransform(at);
+                g2.drawString(yAxisLabel, -yAxisLabelY, yAxisLabelX);
+            }
         };
-        panel.setBackground(Color.WHITE);
+
+        panel.setBackground(Color.BLACK);
         getContentPane().add(panel);
         panel.repaint();
-        setSize(720, 720);
+        setSize(720, 960);
         setVisible(true);
     }
 
