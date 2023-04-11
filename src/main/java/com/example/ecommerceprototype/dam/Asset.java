995fe5b6ae0717b90bc3fa1eb0ed7de3f991ff9c
Sami Gawad <113425328+Sagaw22@users.noreply.github.com>
2023-04-11 10:25:47 +0200

    watermark feature er blevet tilføjet

--- src/main/java/com/example/ecommerceprototype/dam/Asset.java
+++ src/main/java/com/example/ecommerceprototype/dam/Asset.java
@@ -1,6 +1,10 @@
 package com.example.ecommerceprototype.dam;
 
+import java.io.File;
 import java.util.Date;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.*;
 
 public class Asset {
     private int id;
@@ -99,6 +103,47 @@ public class Asset {
 
     public void setDate_added(Date date_added) {
         this.date_added = date_added;
+    }
+
+    public void addWatermark(String mark) throws Exception {
+
+        // vi starter med at indlæse filen
+        File toBeWatermarked = new File(this.filepath);
+
+        BufferedImage originaltBillede = null;
+        try {
+            originaltBillede = ImageIO.read(toBeWatermarked);
+        } catch (Exception e) {
+            throw new Exception("Filen kunne ikke indlæses" + e.getMessage());
+        }
+
+        // nu opretter vi et BufferedImage object til det billede som vi ønsker at tilføje et vandmærke til
+        BufferedImage watermarkedImage = new BufferedImage(originaltBillede.getWidth(), originaltBillede.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        // derefter opretter vi et Graphics2D objekt af billedet vi ønsker at vandmærke
+        Graphics2D g2d = (Graphics2D) watermarkedImage.getGraphics();
+
+        // vi tilføjer og tegner nu det originale billede ovenpå det nye billede
+        g2d.drawImage(originaltBillede, 0, 0, null);
+
+        // herfter kan vi tegne vandmærket ovenpå det nye billede
+        Font font = new Font("Purisa", Font.PLAIN, 48);
+        Color color = Color.WHITE;
+        g2d.setFont(font);
+        g2d.setColor(color);
+        g2d.drawString(mark, 10, 50);
+
+        // vi gemmer nu billedet med vandmærket som en ny fil
+        String watermarkedFilePath = this.filepath + ".watermarked.jpg";
+
+        try{
+            ImageIO.write(watermarkedImage, "jpg", new File(watermarkedFilePath));
+        } catch (Exception e) {
+            throw new Exception("Det vandmærkede billede kunne ikke gemmes " + e.getMessage());
+        }
+
+        // til allersidst opdaterer vi dette assets eksisterende filepath med den nye og vandmærkede fil
+        this.filepath = watermarkedFilePath;
     }
 
 }
