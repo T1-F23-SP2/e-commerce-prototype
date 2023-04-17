421d51d455a81413b69045e7a4f318e3e646c879
Redacted for Privacy <06933b03@hidden.mail>
2023-04-17 09:23:58 +0200

    Removed changes in HelloApplication

--- src/main/java/com/example/ecommerceprototype/HelloApplication.java
+++ src/main/java/com/example/ecommerceprototype/HelloApplication.java
@@ -13,24 +13,11 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cms/ArticlePage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1000);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
-        Pane test = CMS.getInstance().fetchComponent("ArticlePage");
-        ArrayList<String> compList = CMS.getInstance().getComponentList(test);
-        for(String s : compList)
-            System.out.println(s);
-
-        System.out.println("\n");
-
-        System.out.println("\n");
-
-        System.out.println("ArticleCount: " + CMS.getInstance().articles.getArticleCount());
-
-        stage.setScene(new Scene(CMS.getInstance().articles.fetchArticle("DefaultArticle"), 1920, 1000));
     }
 
     public static void main(String[] args) {
