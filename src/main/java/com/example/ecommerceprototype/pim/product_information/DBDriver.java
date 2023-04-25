7fe4808ee663a72ee16d485f7e1b9f309c0919f1
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-04-25 15:13:00 +0200

    Initial commit of database implementation
    This commit contains editing and/or adding of 3 files.
    
    DBDriver was edited, adding all its methods and comments for how they should be called.
    
    database_database_initialization.sql contains the tables, procedures and functions for the database. It still needs the implementation of a few delete methods, but they will be added in a future implementation.
    
    custom_data_for_database.sql contains some custom data for the database so the methods can be tested. The data is not intended for use in the final project, but only as a way for us to test the methods.

--- src/main/java/com/example/ecommerceprototype/pim/product_information/DBDriver.java
+++ src/main/java/com/example/ecommerceprototype/pim/product_information/DBDriver.java
@@ -2,62 +2,253 @@ package com.example.ecommerceprototype.pim.product_information;
 
 import com.example.ecommerceprototype.pim.sql_helpers.SQLValueArguments;
 
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DBDriver {
-    public DBDriver() {
+
+    protected ProductInformation getProductByUUID(SQLValueArguments uuid) {
+        // SQL function: getProductByUUID(argUUID UUID)
+        // Call by: SELECT * FROM getProductByUUID('some-uuid');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected ProductInformation getProductWhereUUIDEqualsX(SQLValueArguments x) {
+    protected ProductInformation getProductByName(SQLValueArguments name) {
+        // SQL function: getProductByName(argName TEXT)
+        // Call by: SELECT * FROM getProductByName('some-name');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected ProductInformation getProductWhereNameEqualsX(SQLValueArguments x) {
+    protected List<ProductInformation> getProductsBySerialNumber(SQLValueArguments serialNumber) {
+        // SQL function: getProductsBySerialNumber(argSerialNumber TEXT)
+        // Call by: SELECT * FROM getProductsBySerialNumber('some-serial-number');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereCategoryNameEqualsX (String name) {
+    protected List<ProductInformation> getProductsThatAreHidden() {
+        // SQL function: getProductsThatAreHidden()
+        // Call by: SELECT * FROM getProductsThatAreHidden();
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereCategoryNameEqualsX (ProductCategory pc) {
+    protected List<ProductInformation> getProductsByCategoryName(SQLValueArguments categoryName) {
+        // SQL function: getProductsThatAreHidden(argCategoryName TEXT)
+        // Call by: SELECT * FROM getProductsThatAreHidden('someCategoryName');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereManufactureNameEqualsX (String name) {
+    protected List<ProductInformation> getProductsByManufactureName(SQLValueArguments manufactureName) {
+        // SQL function: getProductsByManufactureName(argManufactureName TEXT)
+        // Call by: SELECT * FROM getProductsByManufactureName('someManufactureName');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereManufactureNameEqualsX (ManufacturingInformation mi) {
+    protected List<ProductInformation> getProductsByDiscountName(SQLValueArguments discountName) {
+        // SQL function: getProductsByDiscountName(argDiscountName TEXT)
+        // Call by: SELECT * FROM getProductsByDiscountName('someDiscountName');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereDiscountNameEqualsX (String name) {
+    protected ProductCategory getCategoryByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getCategoryByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getCategoryByProductUUID('someUUID');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected List<ProductInformation> getProductsWhereDiscountNameEqualsX (DiscountInformation di) {
+    protected ProductCategory getCategoryByName(SQLValueArguments categoryName) {
+        // SQL function: getCategoryByName(argName TEXT)
+        // Call by: SELECT * FROM getCategoryByName('someCategoryName');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected ProductCategory getCategoryWhereNameEqualsX(String name) {
+    protected ProductCategory getCategoryByCategoryID(SQLValueArguments categoryId) {
+        // This method is most relevant for getting the parent category of a category.
+
+        // SQL function: getCategoryByCategoryID(argID INT)
+        // Call by: SELECT * FROM getCategoryByCategoryID('someCategoryId');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected ManufacturingInformation getManufactureByName(String name) {
+    protected ProductSpecification getSpecificationByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getSpecificationByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getSpecificationByProductUUID('someUUID');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected DiscountInformation getDiscountByName(String name) {
+    protected ManufacturingInformation getManufactureByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getManufactureByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getManufactureByProductUUID('someUUID');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected boolean insertNewEntry(String tableName, SQLValueArguments x) {
+    protected ManufacturingInformation getManufactureByName(SQLValueArguments name) {
+        // SQL function: getManufactureByName(argName TEXT)
+        // Call by: SELECT * FROM getManufactureByName('someName');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
 
-    protected boolean updateEntry(String id, SQLValueArguments x) {
+    protected DiscountInformation getDiscountByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getDiscountByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getDiscountByProductUUID('someUUID');
+        // Look at the database_initialization.sql file for return types and return values.
+
         throw new UnsupportedOperationException();
     }
+
+    protected DiscountInformation getDiscountByName(SQLValueArguments name) {
+        // SQL function: getDiscountByName(argName TEXT)
+        // Call by: SELECT * FROM getDiscountByName('someName');
+        // Look at the database_initialization.sql file for return types and return values.
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected BigDecimal getDiscountPercentageByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getDiscountPercentageByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getDiscountPercentageByProductUUID('someUUID');
+        // Returns a NUMERIC value of the discount.
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected List<PriceInformation> getPricesByProductUUID(SQLValueArguments uuid) {
+        // SQL function: getPricesByProductUUID(argUUID UUID)
+        // Call by: SELECT * FROM getPricesByProductUUID('someUUID');
+        // Look at the database_initialization.sql file for return types and return values.
+
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+    protected void insertNewProduct(ProductInformation productInformation) {
+        // SQL function: insertNewProduct(argName VARCHAR, argSerialNumber VARCHAR, argShortDescription VARCHAR, argProductCategoryName VARCHAR, argManufactureName VARCHAR, argLongDescription TEXT)
+        // Call by: SELECT * FROM insertNewProduct('name', 'serialNumber', 'shortDescription', 'productCategoryName', 'manufactureName', 'longDescription');
+        // Look at the database_initialization.sql file for return types and return values.
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewProductCategory(ProductCategory productCategory) {
+        // SQL function: insertNewProduct(argName VARCHAR, argParentCategoryName VARCHAR)
+        // Call by: CALL insertNewProductCategory('name', 'parentCategoryName'); // OBS! The argParentCategoryName should be null if it doesn't have a parent category.
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewManufacture(ManufacturingInformation manufacturingInformation) {
+        // SQL function: insertNewManufacture(argName VARCHAR, argSupportPhone VARCHAR(32), argSupportMail VARCHAR)
+        // Call by: CALL insertNewManufacture('manufactureName', 'supportPhone', 'supportMail');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewDiscount(DiscountInformation discountInformation) {
+        // SQL function: insertNewManufacture(argName VARCHAR, argStartDate TIMESTAMP, argEndDate TIMESTAMP)
+        // Call by: CALL insertNewDiscount('testDiscount', '01-06-2023', '01-07-2023');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewSpecification(ProductSpecification productSpecification) {
+        // SQL function: insertNewManufacture(argProductUUID UUID, argKey VARCHAR, argValue VARCHAR)
+        // Call by: CALL insertNewSpecification('71bce9bd-ef5f-48c2-af68-9e721cf4f181', 'CPU', 'testValue1243');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewPriceChange(SQLValueArguments uuid, SQLValueArguments price, SQLValueArguments wholeSalePrice) {
+        // SQL function: insertNewManufacture(argProductUUID UUID, argPrice NUMERIC, argWholesalePrice NUMERIC)
+        // Call by: CALL insertNewPriceChange('someUUID', 1234, 1234);
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void insertNewPriceChange(SQLValueArguments uuid, SQLValueArguments price, SQLValueArguments wholeSalePrice, DiscountInformation discountInformation) {
+        // SQL function: insertNewManufacture(argProductUUID UUID, argPrice NUMERIC, argWholesalePrice NUMERIC, argDiscountName VARCHAR)
+        // Call by: CALL insertNewPriceChange('someUUID', 1234, 1234, 'discountName');
+
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+    protected void updateProductByUUID(SQLValueArguments uuid, ProductInformation productInformation) {
+        // SQL function: updateProductByUUID(argUUID UUID, argName VARCHAR, argSerialNumber VARCHAR, argShortDescription VARCHAR, argProductCategoryName VARCHAR, argManufactureName VARCHAR, argIsHidden BOOLEAN, argLongDescription TEXT)
+        // Call by: CALL updateProductByUUID('someUUID', 'newName', 'newSerialNumber', 'newShortDescription', 'newCategoryName', 'newManufactureName', false, 'newLongDescription');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void updateManufactureByName(SQLValueArguments name, ProductCategory productCategory) {
+        // SQL function: updateManufactureByName(argName VARCHAR, argNewName VARCHAR, argSupportPhone VARCHAR(32), argSupportMail VARCHAR)
+        // Call by: CALL updateManufactureByName('oldName', 'newName', 'newSupportPhone', 'newSupportMail');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void updateDiscountByName(SQLValueArguments name, ProductCategory productCategory) {
+        // SQL function: updateDiscountByName(argName VARCHAR, argNewName VARCHAR, argStartDate TIMESTAMP, argEndDate TIMESTAMP)
+        // Call by: CALL updateDiscountByName('oldDiscountName', 'newDiscountName', '01-01-2023', '01-02-2023');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void updateProductCategoryByName(SQLValueArguments name, ProductCategory productCategory) {
+        // SQL function: updateProductCategoryByName(argName VARCHAR, argNewName VARCHAR, argParentCategoryName VARCHAR)
+        // Call by: CALL updateProductCategoryByName('oldCategoryName', 'newCategoryName', 'parentCategoryName');
+
+        throw new UnsupportedOperationException();
+    }
+
+    protected void updateSpecificationByProductUUIDAndKey(SQLValueArguments uuid, ProductCategory productCategory) {
+        // SQL function: updateSpecificationByProductUUIDAndKey(argProductUUID UUID, argKey VARCHAR, argNewKey VARCHAR, argNewValue VARCHAR)
+        // Call by: CALL updateSpecificationByProductUUIDAndKey('someUUID', 'oldKeyName', 'newKeyName', 'newValue');
+
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+    // TODO: The following procedures are missing before they can be implementded:
+    // deleteProductByUUID()
+    // deleteCategoryByName()
+    // deleteSpecificationByProductUUID()
+    // deleteManufactureByName()
+    // deletePriceChangeByProductUUID()
+    // deleteDiscountByName()
 }
