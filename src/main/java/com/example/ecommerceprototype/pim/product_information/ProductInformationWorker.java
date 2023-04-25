cfb201f46bc5514bb5e92e95309a5862f0d2453a
Redacted for Privacy <43d703f7@hidden.mail>
2023-04-25 13:06:27 +0200

    Product information worker implementation
    The abstract worker class for product information has been implemented

--- src/main/java/com/example/ecommerceprototype/pim/product_information/ProductInformationWorker.java
+++ src/main/java/com/example/ecommerceprototype/pim/product_information/ProductInformationWorker.java
@@ -4,39 +4,48 @@ import com.example.ecommerceprototype.pim.exceptions.DuplicateEntryException;
 import com.example.ecommerceprototype.pim.exceptions.IncompleteProductCategoryInformation;
 
 public abstract class ProductInformationWorker {
+
+    ProductInformation productInformation;
+
+    public ProductInformationWorker() {
+        this.productInformation = new ProductInformation()
+    }
+
     public void setProductCategory(ProductCategory pc) {
-        throw new UnsupportedOperationException();
+        this.productInformation.setProductCategory(pc);
     }
 
     public void setProductSpecification(ProductSpecification ps) {
-        throw new UnsupportedOperationException();
+        this.productInformation.setProductSpecification(ps);
     }
 
     public void setManufacturingInformation(ManufacturingInformation mi) {
-        throw new UnsupportedOperationException();
+        this.productInformation.setManufacturingInformation(mi);
     }
 
-    public void setName(String s) {
-        throw new UnsupportedOperationException();
+    public void setPriceInformation(PriceInformation pi) {
+        this.productInformation.setPriceInformation(pi);
     }
 
-    public void setShortDescription(String s) {
-        throw new UnsupportedOperationException();
+    public void setName(String name) {
+        this.productInformation.setName(name);
     }
 
-    public void setLongDescription(String s) {
-        throw new UnsupportedOperationException();
+    public void setShortDescription(String shortDescription) {
+        this.productInformation.setShortDescription(shortDescription);
     }
 
-    public void setIsHidden(String s) {
-        throw new UnsupportedOperationException();
+    public void setLongDescription(String longDescription) {
+        this.productInformation.setLongDescription(longDescription);
+    }
+
+    public void setIsHidden(boolean isHidden) {
+        this.productInformation.setIsHidden(isHidden);
     }
 
     public ProductInformation getProductInformation() {
-        throw new UnsupportedOperationException();
+        return productInformation;
     }
 
-    public DiscountInformation submit() throws DuplicateEntryException, IncompleteProductCategoryInformation {
-        throw new UnsupportedOperationException();
-    }
+    public abstract DiscountInformation submit() throws DuplicateEntryException, IncompleteProductCategoryInformation;
 }
