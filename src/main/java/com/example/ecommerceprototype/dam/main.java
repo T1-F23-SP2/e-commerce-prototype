f3992c8703082c12d201085a0b30bd188f314aab
Sami Gawad <113425328+Sagaw22@users.noreply.github.com>
2023-04-25 12:26:05 +0200

    Added to main method

--- src/main/java/com/example/ecommerceprototype/dam/main.java
+++ src/main/java/com/example/ecommerceprototype/dam/main.java
@@ -1,13 +1,29 @@
 package com.example.ecommerceprototype.dam;
-
+import com.example.ecommerceprototype.dam.FileSystem;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import java.util.*;
+import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 import java.util.Date;
 
-public class main {
-    public static void main(String[] args) {
+public class main extends Application {
+
+    public void start(Stage stage) throws IOException {
         DAM test = new DAM();
-
-
-
-
+        Application test2 = new GUI();
+        Application.launch();
     }
 }
