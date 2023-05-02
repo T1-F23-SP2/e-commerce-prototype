a5c15a4826fdb99be963e9210101cf527e7ac0e2
Sami Gawad <113425328+Sagaw22@users.noreply.github.com>
2023-05-02 14:06:09 +0200

    Opdatering på GUI

--- src/main/java/com/example/ecommerceprototype/dam/LoginControl.java
+++ src/main/java/com/example/ecommerceprototype/dam/LoginControl.java
@@ -1,13 +1,20 @@
 package com.example.ecommerceprototype.dam;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Window;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 public class LoginControl {
+
+    DAMController dam = new DAMController();
 
     @FXML
     private TextField UsernameField;
@@ -19,21 +26,54 @@ public class LoginControl {
     private Button loginButton;
 
     @FXML
-    public void login(javafx.event.ActionEvent actionEvent){
+    private Button quitButton;
+
+    @FXML
+    private Hyperlink forgotPass;
+
+    @FXML
+    public void forgotPassClicked(javafx.event.ActionEvent event){
+        showAlert(Alert.AlertType.INFORMATION, forgotPass.getScene().getWindow(), "Forgot Password",
+                "skill issue");
+    }
+
+    @FXML
+    public void quitButtonClicked(javafx.event.ActionEvent quitEvent){
+        Platform.exit();
+    }
+
+    @FXML
+    public void login(javafx.event.ActionEvent actionEvent) throws IOException {
         Window owner = loginButton.getScene().getWindow();
 
         System.out.println(UsernameField.getText());
         System.out.println(passwordField.getText());
 
         if (UsernameField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "Please enter your email id");
+            showAlert(Alert.AlertType.ERROR, owner, "Error!",
+                    "Please enter your username");
             return;
         }
         if (passwordField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+            showAlert(Alert.AlertType.ERROR, owner, "Error!",
                     "Please enter a password");
             return;
+        }
+
+        if (UsernameField.getText().equals("admin") &&
+                passwordField.getText().contains("banan")) {
+
+            // her tjekker den om credentials er rigtige
+
+            infoBox("Login successful!","Success","Login");
+            dam.switchToDAM(actionEvent);
+
+            // tilføj kode således at vi kan move til next stage
+            // Stage stage = osv...
+            // stage.setScene(new Scene(root))
+
+        } else {
+            showAlert(Alert.AlertType.ERROR, owner, "Error!","Invalid username or password" );
         }
     }
 
