393938f5a3c4c72bd3e6a6e38f426cdf1b58cf5c
MidnightRocket <git@midnightrocket.dev>
2023-05-09 11:54:32 +0200

    Rename SQLConnectionTest to SQLExampleTest
    This is because the test currently is just an example of how to run
    tests on a test database.

--- src/test/java/com/example/ecommerceprototype/pim/sql_helpers/SQLExampleTest.java
+++ src/test/java/com/example/ecommerceprototype/pim/sql_helpers/SQLExampleTest.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SQLConnectionTest {
+class SQLExampleTest {
 
     static Connection connection;
     static TestConnectionWrapper testConnectionWrapper;
