929c84abcfd6feb0f0eabb7041b03bd3257a7944
looseMole <62012079+looseMole@users.noreply.github.com>
2023-05-11 17:20:50 +0200

    Create tests for rest of refactored get-methods

--- src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverTest.java
+++ src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverTest.java
@@ -1,5 +1,6 @@
 package com.example.ecommerceprototype.pim.product_information;
 
+import com.example.ecommerceprototype.pim.exceptions.NotFoundException;
 import com.example.ecommerceprototype.pim.sql_helpers.SQLConnectionTestInitializerTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -107,7 +108,6 @@ public class DBDriverTest {
                 productInformationArrayList.get(productInformationArrayList.size()-1).getProductUUID()
                         .equals(returnedList.get(returnedList.size()-1).getProductUUID()));
     }
-
     @Test
     void testGetProductsThatAreHidden() {
         ArrayList<ProductInformation> productInformationArrayList = new ArrayList<>();
@@ -146,15 +146,272 @@ public class DBDriverTest {
                 productInformationArrayList.get(0).getProductUUID().equals(returnedList.get(0).getProductUUID()) &&
                 productInformationArrayList.get(productInformationArrayList.size()-1).getProductUUID()
                         .equals(returnedList.get(returnedList.size()-1).getProductUUID()));
+
+        // Remove inserted, hidden products.
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM products WHERE id=12; DELETE FROM products WHERE id=13");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Test
-    void testNoProductThatIsHidden() {
+    void testGetNoProductThatIsHidden() {
         ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsThatAreHidden();
 
         assertTrue(returnedList.isEmpty());
     }
-    // TODO: Create tests for the rest of the refactored get-methods.
+
+    @Test
+    void testGetProductsByCategoryName() {
+        ArrayList<ProductInformation> productInformationArrayList = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductsByCategoryName(?)");
+            preparedStatement.setString(1, "TV");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            while (resultSet.next()) {
+
+                ProductInformation productInformation = new ProductInformation();
+
+                productInformation.setUUID(resultSet.getString("product_UUID"))
+                        .setName(resultSet.getString("name"))
+                        .setSerialNumber(resultSet.getString("serial_number"))
+                        .setShortDescription(resultSet.getString("short_description"))
+                        .setIsHidden(resultSet.getBoolean("is_hidden"))
+                        .setLongDescription(resultSet.getString("long_description"));
+
+                productInformationArrayList.add(productInformation);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByCategoryName("TV");
+
+        assertTrue(productInformationArrayList.size() == returnedList.size() &&
+                productInformationArrayList.get(0).getProductUUID().equals(returnedList.get(0).getProductUUID()) &&
+                productInformationArrayList.get(productInformationArrayList.size()-1).getProductUUID()
+                        .equals(returnedList.get(returnedList.size()-1).getProductUUID()));
+    }
+
+    @Test
+    void testGetNoProductByCategoryName() {
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByCategoryName("PC & tablets");
+
+        assertTrue(returnedList.isEmpty());
+    }
+
+    @Test
+    void testGetProductsByManufactureName() {
+        ArrayList<ProductInformation> productInformationArrayList = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductsByMAnufactureName(?)");
+            preparedStatement.setString(1, "Lenovo");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            while (resultSet.next()) {
+
+                ProductInformation productInformation = new ProductInformation();
+
+                productInformation.setUUID(resultSet.getString("product_UUID"))
+                        .setName(resultSet.getString("name"))
+                        .setSerialNumber(resultSet.getString("serial_number"))
+                        .setShortDescription(resultSet.getString("short_description"))
+                        .setIsHidden(resultSet.getBoolean("is_hidden"))
+                        .setLongDescription(resultSet.getString("long_description"));
+
+                productInformationArrayList.add(productInformation);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByManufactureName("Lenovo");
+
+        assertTrue(productInformationArrayList.size() == returnedList.size() &&
+                productInformationArrayList.get(0).getProductUUID().equals(returnedList.get(0).getProductUUID()) &&
+                productInformationArrayList.get(productInformationArrayList.size()-1).getProductUUID()
+                        .equals(returnedList.get(returnedList.size()-1).getProductUUID()));
+    }
+
+    @Test
+    void testGetNoProductByManufactureName() {
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Manufactures (id, name, support_phone, support_mail) " +
+                    "VALUES (10, 'NAN', 21212121, 'NAN@mail.com')");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.toString());
+        }
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByManufactureName("NAN");
+
+        assertTrue(returnedList.isEmpty());
+    }
+
+    @Test
+    void testGetProductsByDiscountName() {
+        ArrayList<ProductInformation> productInformationArrayList = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductsByDiscountName(?)");
+            preparedStatement.setString(1, "Spring sale");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            while (resultSet.next()) {
+
+                ProductInformation productInformation = new ProductInformation();
+
+                productInformation.setUUID(resultSet.getString("product_UUID"))
+                        .setName(resultSet.getString("name"))
+                        .setSerialNumber(resultSet.getString("serial_number"))
+                        .setShortDescription(resultSet.getString("short_description"))
+                        .setIsHidden(resultSet.getBoolean("is_hidden"))
+                        .setLongDescription(resultSet.getString("long_description"));
+
+                productInformationArrayList.add(productInformation);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByDiscountName("Spring sale");
+
+        assertTrue(productInformationArrayList.size() == returnedList.size() &&
+                productInformationArrayList.get(0).getProductUUID().equals(returnedList.get(0).getProductUUID()) &&
+                productInformationArrayList.get(productInformationArrayList.size()-1).getProductUUID()
+                        .equals(returnedList.get(returnedList.size()-1).getProductUUID()));
+    }
+
+    @Test
+    void testGetNoProductByDiscountName() {
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Discounts (id, name, start_date, end_date) " +
+                    "VALUES (10, 'NAN', '2023-05-11 16:18:28.388766', '2023-06-01 00:00:00.000000')");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.toString());
+        }
+        ArrayList<ProductInformation> returnedList = (ArrayList<ProductInformation>) dbDriver.getProductsByManufactureName("NAN");
+
+        assertTrue(returnedList.isEmpty());
+    }
+
+    @Test
+    void testGetCategoryByProductUUID() {
+        String uuid = "";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductById(?)");
+            preparedStatement.setInt(1, 10);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            uuid = resultSet.getString("product_uuid");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("TV", dbDriver.getCategoryByProductUUID(uuid).getName());
+    }
+
+    @Test
+    void testGetCategoryByCategoryID() {
+        assertEquals("TV", dbDriver.getCategoryByCategoryID(8).getName());
+    }
+
+    @Test
+    void testGetCategoryByName() {
+        assertEquals(dbDriver.getCategoryByCategoryID(8).getName(), dbDriver.getCategoryByName("TV").getName());
+    }
+
+    @Test
+    void testGetSpecificationByProductUUID() {
+        String uuid = "";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductById(?)");
+            preparedStatement.setInt(1, 1);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            uuid = resultSet.getString("product_uuid");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("AMD Ryzen 5600U", dbDriver.getSpecificationByProductUUID(uuid).get("CPU"));
+    }
+
+    @Test
+    void testGetManufactureByProductUUID() {
+        String uuid = "";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductById(?)");
+            preparedStatement.setInt(1, 1);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            uuid = resultSet.getString("product_uuid");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("Lenovo", dbDriver.getManufactureByProductUUID(uuid).getName());
+    }
+
+    @Test
+    void testGetManufactureByName() {
+        assertEquals("12345678", dbDriver.getManufactureByName("Lenovo").getSupportPhoneNumber());
+    }
+
+    @Test
+    void testGetDiscountByProductUUID() {
+        String uuid = "";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT product_uuid FROM products WHERE id = ?");
+            preparedStatement.setInt(1, 1);
+            preparedStatement.execute();
+
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            uuid = resultSet.getString("product_uuid");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("Spring sale", dbDriver.getDiscountByProductUUID(uuid).getName());
+    }
+
+    @Test
+    void testGetDiscountByName() { // Boring looking test - but then again, if no exception occurs, isn't it technically working?
+        try {
+            assertEquals("Spring sale", dbDriver.getDiscountByName("Spring sale").getName());
+        } catch (NotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testGetNoDiscountByName() { // Boring looking test - but then again, if no exception occurs, isn't it technically working?
+        assertThrows(
+                NotFoundException.class,
+                () -> dbDriver.getDiscountByName("NotRealDiscount!")
+                );
+    }
 
     @AfterAll
     static void end() {
