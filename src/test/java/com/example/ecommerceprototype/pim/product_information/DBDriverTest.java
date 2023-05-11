0ecc645fcadab18c96545282d30d5632d2815a0e
looseMole <62012079+looseMole@users.noreply.github.com>
2023-05-11 14:00:26 +0200

    Create first 2 getProduct-tests

--- src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverTest.java
+++ src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverTest.java
@@ -25,4 +25,51 @@ public class DBDriverTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    void testGetProductByName() {
+        String expectedUUID = "";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductByName(?)");
+            preparedStatement.setString(1, "Lenovo Ideapad 5 Pro 14\" QHD touch");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            expectedUUID = resultSet.getString("product_UUID");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ProductInformation returnedProduct = dbDriver.getProductByName("Lenovo Ideapad 5 Pro 14\" QHD touch");
+
+        assertEquals(expectedUUID, returnedProduct.getProductUUID());
+    }
+
+    @Test
+    void testGetProductByUUID() {
+        String expectedName = "";
+        String uuid = "";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM getProductById(?)");
+            preparedStatement.setInt(1, 1);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            expectedName = resultSet.getString("name");
+            uuid = resultSet.getString("product_uuid");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ProductInformation returnedProduct = dbDriver.getProductByUUID(uuid);
+
+        assertEquals(expectedName, returnedProduct.getName());
+    }
+
+    // TODO: Create tests for the rest of the refactored get-methods.
+
 }
