ba02ed54b151e187aba0bb9406bc9c5c30eda0e3
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-05-12 02:51:19 +0200

    Added exports to module-info.java file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,9 @@ module com.example.ecommerceprototype {
 
     opens com.example.ecommerceprototype to javafx.fxml;
     exports com.example.ecommerceprototype;
+    exports com.example.ecommerceprototype.pim.product_information;
+    exports com.example.ecommerceprototype.pim.presentation;
+    exports com.example.ecommerceprototype.pim.exceptions;
+    exports com.example.ecommerceprototype.pim.sql_helpers;
+    opens com.example.ecommerceprototype.pim.presentation to javafx.fxml;
 }
