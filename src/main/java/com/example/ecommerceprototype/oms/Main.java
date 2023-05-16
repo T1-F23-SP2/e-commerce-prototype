fd83e750f833898a1c1e67cfd7fe6a119f83a618
Redacted for Privacy <81077a58@hidden.mail>
2023-05-16 14:13:09 +0200

    Moved some files into proper directories

---
+++ src/main/java/com/example/ecommerceprototype/oms/Main.java
@@ -0,0 +1,21 @@
+package com.example.ecommerceprototype.oms;
+
+import DB.DBManager;
+import DB.StockInterface;
+import MockShop.PlaceholderInstShop;
+import Visuals.TableCreator;
+import com.mongodb.client.MongoCollection;
+import mockPIM.PlaceHolderInstGet;
+import org.bson.Document;
+import ComputedOverviews.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main implements StockInterface {
+    public static void main(String[] args) {
+
+    }
+}
