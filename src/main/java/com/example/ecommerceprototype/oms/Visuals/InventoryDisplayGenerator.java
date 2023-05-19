0c5a2c660c89a65a86623fd41b63158c9d062da4
Redacted for Privacy <81077a58@hidden.mail>
2023-05-19 16:41:53 +0200

    Added inventoryDisplayGenerator class + test

---
+++ src/main/java/com/example/ecommerceprototype/oms/Visuals/InventoryDisplayGenerator.java
@@ -0,0 +1,83 @@
+package com.example.ecommerceprototype.oms.Visuals;
+
+import com.example.ecommerceprototype.oms.ComputedOverviews.SalesReport;
+import com.example.ecommerceprototype.oms.mockPIM.PlaceHolderInstGet;
+import com.itextpdf.awt.DefaultFontMapper;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.*;
+import org.jfree.chart.JFreeChart;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.FileOutputStream;
+import java.util.List;
+
+public class InventoryDisplayGenerator {
+
+
+    static BaseColor color2 = new BaseColor(222, 213, 12);
+
+    public static PdfPTable O_Table() {
+        // Change this to the right column names
+        String[] columnHeaders = {"Name", "QTY", "UUID"};
+
+        PdfPTable O_table = new PdfPTable(columnHeaders.length);
+        O_table.setWidthPercentage(100);
+
+        for (String columnHeader : columnHeaders) {
+            PdfPCell headerCell = new PdfPCell(new Phrase(columnHeader));
+            headerCell.setBackgroundColor(color2);
+            O_table.addCell(headerCell);
+        }
+
+        for (int j = 0; j < PlaceHolderInstGet.productArray.length; j++) {
+
+            PdfPCell cell_name = new PdfPCell(new Phrase(PlaceHolderInstGet.productArray[j].getName()));
+            O_table.addCell(cell_name);
+
+            PdfPCell cell_itemQTY = new PdfPCell(new Phrase(((SalesReport.getQTY(PlaceHolderInstGet.productArray[j].getProductUUID())).toString())));
+            O_table.addCell(cell_itemQTY);
+
+            PdfPCell cell_UUID = new PdfPCell(new Phrase(PlaceHolderInstGet.productArray[j].getProductUUID()));
+            O_table.addCell(cell_UUID);
+
+
+        }
+        return O_table;
+    }
+
+    public static void convertToPdf(int width, int height, String filename) {
+        //Initialize document
+        Document document = new Document(PageSize.A4);
+        try {
+            // Initialize writer
+            PdfWriter O_Writer;
+            // Calls instance of document with the file output
+            O_Writer = PdfWriter.getInstance(document, new FileOutputStream(filename));
+            //Open document to write to the pdf
+            document.open();
+
+
+            // Adding the FUCKTable to the pdf - (CellTable)
+            PdfPTable table = O_Table();
+
+            document.add(table);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Close document
+        document.close();
+
+
+    }
+
+    public static void main(String[] args) {
+        int width = 300;
+        int height = 350;
+        String fileName = "assets/oms/out/Overview.pdf";
+        convertToPdf(width, height, fileName);
+    }
+}
+
--- src/test/java/com/example/ecommerceprototype/oms/visuals/SalesReportGeneratorTest.java
+++ src/test/java/com/example/ecommerceprototype/oms/visuals/SalesReportGeneratorTest.java
@@ -12,9 +12,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.example.ecommerceprototype.oms.Visuals.SalesReportGenerator.pdfMaker;
 import static org.junit.jupiter.api.Assertions.*;
 
-class FOOTest {
+class SalesReportGeneratorTest {
 
     String uri = "mongodb+srv://Kristoffer:123456789A@testerinvoice.t8c16zx.mongodb.net/test";
     MongoClient mongoClient = MongoClients.create(uri);
@@ -35,31 +36,43 @@ class FOOTest {
     @Test
     void DB() {
         //Tester at der bliver hentet UUID fra database
-        Document result = collection.find(Filters.eq("UUID","1U2U3I4D1")).first();
+        Document result = collection.find(Filters.eq("UUID", "1U2U3I4D1")).first();
         System.out.println(result.toJson());
-        assertEquals("{\"_id\": {\"$oid\": \"643e9afcced88a178463cfb2\"}, \"UUID\": \"1U2U3I4D1\", \"QTY\": 10}", result.toJson());
+        assertEquals("{\"_id\": {\"$oid\": \"643e9afcced88a178463cfb2\"}, \"UUID\": \"1U2U3I4D1\", \"QTY\": 25}", result.toJson());
     }
 
     @Test
-    void FUCKTable() {
+    void Table_Table() {
         // Tester at der er indhold i pdf
         table.addCell(headerCell);
         assertTrue(table.isContent());
     }
 
     @Test
-    void FUCK() {
+    void Chart_BARchart() {
     }
 
     @Test
-    void FUCKPIE() {
+    void Chart_PIEchart() {
     }
+
 
     @Test
     void convertToPdf() {
+        long Starttime = System.currentTimeMillis();
+
+        pdfMaker();
+
+        long EndTime = System.currentTimeMillis();
+        long generationTime = EndTime - Starttime;
+        assertLessThanOrEqual(generationTime, 10000);
     }
 
-    @Test
-    void main() {
+    private void assertLessThanOrEqual(long actual, long expected) {
+        if (actual > expected) {
+            throw new AssertionError("Expected generation time to be less than or equal to " + expected + "ms, but it took " + actual + "ms.");
+        }
+
+
     }
 }
