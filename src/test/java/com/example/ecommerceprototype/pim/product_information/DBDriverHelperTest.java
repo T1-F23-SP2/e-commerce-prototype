85c762ecabcc65e68e56dc2c9b87124c0b4ddc25
MidnightRocket <git@midnightrocket.dev>
2023-05-28 17:46:33 +0200

    Moved content from DBDriverChecksTest to DBDriverHelperTest

--- src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverHelperTest.java
+++ src/test/java/com/example/ecommerceprototype/pim/product_information/DBDriverHelperTest.java
@@ -1,17 +1,28 @@
 package com.example.ecommerceprototype.pim.product_information;
 
+import com.example.ecommerceprototype.pim.exceptions.CategoryNotFoundException;
+import com.example.ecommerceprototype.pim.exceptions.UUIDNotFoundException;
+import com.example.ecommerceprototype.pim.util.ProductList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Tests for helper methods in DBDriver")
 public class DBDriverHelperTest extends DBDriverAbstractTest {
     @Test
-    void testCheckIfProductByUUIDNotExists() throws SQLException {
+    void testCheckIfProductByUUIDNotExists() throws SQLException, UUIDNotFoundException, CategoryNotFoundException {
         assertFalse(pimDriver.checkIfProductByUUIDExists("NotARealUUID!"));
+
+        ProductList productList = pimDriver.getAllProducts();
+        ProductInformation pi = productList.get(0);
+        String uuid = pi.getProductUUID();
+
+
+        assertTrue(pimDriver.checkIfProductByUUIDExists(uuid));
     }
 
     @Test
